fedfc2567dfd6eb97ec26853a45b67a6a32917bf
SoWave <piotr.mikolajczyk364@gmail.com>
2021-01-30 15:55:56 +0100

    moved psql info into dsn flag, and added session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,43 +4,38 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"html/template"
+	"time"
 
 	"github.com/SoWave/snippetbox/pkg/models/psql"
 
+	"github.com/golangcollege/sessions"
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "web"
-	password = "pass"
-	dbname   = "snippetbox"
-)
-
+// Application configurration
 type application struct {
 	errorLog      *log.Logger
 	infoLog       *log.Logger
+	session       *sessions.Session
 	snippets      *psql.SnippetModel
 	templateCache map[string]*template.Template
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "host=localhost port=5432 user=web "+
+		"password=pass dbname=snippetbox sslmode=disable", "PostgreSQL info")
+	secret := flag.String("secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge", "Secret session key")
 	flag.Parse()
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	db, err := openDB(psqlInfo)
+	db, err := openDB(*dsn)
 	if err != nil {
 		panic(err)
 	}
@@ -50,11 +45,14 @@ func main() {
 	if err != nil {
 		errorLog.Fatal(err)
 	}
-	fmt.Println(templateCache)
+
+	session := sessions.New([]byte(*secret))
+	session.Lifetime = 12 * time.Hour
 
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
+		session:       session,
 		snippets:      &psql.SnippetModel{DB: db},
 		templateCache: templateCache,
 	}
