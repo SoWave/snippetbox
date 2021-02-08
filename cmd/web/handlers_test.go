90542a42d3f3e6f9fd4d16c4ee1f108e30a346e3
SoWave <piotr.mikolajczyk364@gmail.com>
2021-02-08 19:18:54 +0100

    Created showsnippet test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -21,4 +22,36 @@ func TestPing(t *testing.T) {
 	}
 }
 
+func TestShowSnippet(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
+	testCases := []struct {
+		name string
+		urlPath string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID", "/snippet/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Non-existent ID", "/snippet/2", http.StatusNotFound, nil},
+		{"Negative ID", "/snippet/-1", http.StatusNotFound, nil},
+		{"Decimal ID", "/snippet/1.23", http.StatusNotFound, nil},
+		{"String ID", "/snippet/foo", http.StatusNotFound, nil},
+		{"Empty ID", "/snippet/", http.StatusNotFound, nil},
+		{"Trailing slash", "/snippet/1/", http.StatusNotFound, nil},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tC.urlPath)
+
+			if code != tC.wantCode {
+				t.Errorf("want %d; got %d", tC.wantCode, code)
+			}
+
+			if !bytes.Contains(body, tC.wantBody) {
+				t.Errorf("want body to contain %q", tC.wantBody)
+			}
+		})
+	}
+}
